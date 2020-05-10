3c59ce38dd9c96f556eed50869a05c72b5626424
Justice Amoah Mensah <justice.amensah48@gmail.com>
2020-05-10 04:12:39 +0000

    upgrade on all files

--- app/src/main/java/com/amjustice/braintrainer/MainActivity.java
+++ app/src/main/java/com/amjustice/braintrainer/MainActivity.java
@@ -1,7 +1,7 @@
 package com.amjustice.braintrainer;
 
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
